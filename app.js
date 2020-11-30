d0d17d3a53035adb4db2533ad63e627c408656e9
Michael-Bistou <mimoaz@hotmail.com>
2020-11-30 12:39:50 +0100

    1st commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const phoneUsersRouter = require('./routes/users');
+const phoneApiRouter = require('./routes/phoneApi');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/phone', phoneApiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
